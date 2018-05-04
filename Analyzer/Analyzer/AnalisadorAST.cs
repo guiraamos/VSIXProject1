08f09123f6888e0f6ad13725d71bdff3c1f6eb46
Guilherme Ramos <guilhermermos@gmail.com>
2018-05-04 18:24:24 -0300

    .
    .

--- Analyzer/Analyzer/AnalisadorAST.cs
+++ Analyzer/Analyzer/AnalisadorAST.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System;
+using System.Collections;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace CodeAnalysisApp
 {
     public class AnalisadorAST
     {
+        private static readonly string NameSpacetextService = "Service";
+        private static readonly string[] DependenciasService = { "System.Collections.Generic", "MicroServiceNet", "RestSharp" };
+
         public static void Analisar(string classeText)
         {
             SyntaxTree tree = CSharpSyntaxTree.ParseText(classeText);
@@ -19,19 +26,109 @@ namespace CodeAnalysisApp
             {
                 foreach (var item in method.ChildNodes())
                 {
-                    if(item.GetType().Name == "BlockSyntax")
+                    if (item is BlockSyntax)
                     {
-                        foreach (var variaveis in item.ChildNodes())
-                        {
-                            if(variaveis.GetType().Equals(typeof(LocalDeclarationStatementSyntax)))
-                            {
-
-                            }
-                        }
+                        NavegaEntreNodosDoMetodo(item.ChildNodes());
                     }
                 }
             }
 
+        }
+
+        private static void NavegaEntreNodosDoMetodo(IEnumerable<SyntaxNode> nodes)
+        {
+            foreach (var nodo in nodes)
+            {
+                if (nodo is VariableDeclarationSyntax)
+                {
+                    IdentifierNameSyntax id = (IdentifierNameSyntax)nodo.ChildNodes().FirstOrDefault(n => n is IdentifierNameSyntax);
+                    VariableDeclaratorSyntax declaracao = (VariableDeclaratorSyntax)nodo.ChildNodes().FirstOrDefault(n => n is VariableDeclaratorSyntax);
+
+                    if (declaracao.Initializer.Value.ToString().Contains("PostAsync"))
+                    {
+                        MontaNovaClassePost(nodo);
+                    }
+
+                    if (declaracao.Initializer.Value.ToString().Contains("GetAsync"))
+                    {
+                        MontaNovaClasseGet(nodo);
+                    }
+                }
+                NavegaEntreNodosDoMetodo(nodo.ChildNodes());
+            }
+        }
+
+        private static void MontaNovaClasseGet(SyntaxNode nodo)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static void MontaNovaClassePost(SyntaxNode nodo)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        static void CreateClassService(string nameClass, string hostMicroServico, string nameOfmethod)
+        {
+            //Cria um namespace da classe (namespace CodeGenerationSample)
+            var @namespace = SyntaxFactory.NamespaceDeclaration(SyntaxFactory.ParseName(NameSpacetextService)).NormalizeWhitespace();
+
+            // Add as dependencias da classe
+            foreach (string dependence in DependenciasService)
+            {
+                @namespace = @namespace.AddUsings(SyntaxFactory.UsingDirective(SyntaxFactory.ParseName(dependence)));
+            }
+
+            //  Cria a classe
+            var classDeclaration = SyntaxFactory.ClassDeclaration(nameClass);
+
+            // Torna a classe pública
+            classDeclaration = classDeclaration.AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword));
+
+            // Adiciona a herânca MicroServiceBase a classe
+            classDeclaration = classDeclaration.AddBaseListTypes(SyntaxFactory.SimpleBaseType(SyntaxFactory.ParseTypeName("MicroServiceBase")));
+
+
+            // Add a tag MicroServiceHost com o valor do HOST encontrado
+            var attribute = SyntaxFactory.Attribute(SyntaxFactory.ParseName("MicroServiceHost"), SyntaxFactory.ParseAttributeArgumentList(hostMicroServico));
+            var attributeList = SyntaxFactory.AttributeList(SyntaxFactory.SeparatedList<AttributeSyntax>().Add(attribute));
+            classDeclaration = classDeclaration.AddAttributeLists(attributeList);
+
+
+
+            // Create a method
+            var methodDeclaration = SyntaxFactory
+                .MethodDeclaration(SyntaxFactory.ParseTypeName("IRestResponse"), nameOfmethod)
+                .AddModifiers(SyntaxFactory.Token(SyntaxKind.StaticKeyword))
+                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+                .AddParameterListParameters(SyntaxFactory.Parameter(SyntaxFactory.Identifier("parameters"))
+                        .WithType(SyntaxFactory.ParseTypeName(typeof(List<KeyValuePair<object, object>>).FullName))
+                        .WithDefault(SyntaxFactory.EqualsValueClause(SyntaxFactory.LiteralExpression(SyntaxKind.NullLiteralExpression))));
+
+
+            // Add the field, the property and method to the class.
+            classDeclaration = classDeclaration.AddMembers(methodDeclaration);
+
+            // Add the class to the namespace.
+            @namespace = @namespace.AddMembers(classDeclaration);
+
+
+
+
+
+
+
+
+
+
+            // Normalize and get code as string.
+            var code = @namespace
+                .NormalizeWhitespace()
+                .ToFullString();
+
+            // Output new code to the console.
+            Console.WriteLine(code);
         }
     }
 }
